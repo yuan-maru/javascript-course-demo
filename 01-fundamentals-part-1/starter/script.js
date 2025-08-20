007b70c5ca46abbdf8bb2eb76cdf54ab157004e6
yuan-maru <estabillo.maruyuan@gmail.com>
2025-08-20 14:19:09 +0800

    feat: implement operators and expressions
    - Add mathematical operators (+, -, *, /, **)
    - Implement assignment operators (+=, -=, *=, /=, ++, --)
    - Create comparison operators for boolean logic
    - Understand operator precedence and parentheses
    - Complete BMI Calculator coding challenge with real-world application

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,23 +1,69 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+// // JavaScript Fundamentals - Part 1
+// // We'll write our code here!
 
-let js = "amazing";
-console.log(40 +8 + 23 - 10);
-console.log("GUMANA KA HEHE");
+// let js = "amazing";
+// console.log(40 +8 + 23 - 10);
+// console.log("GUMANA KA HEHE");
 
 
-console.log("=== VARIABLES ===");
+// console.log("=== VARIABLES ===");
 
-let firstName ="Jonas";
-console.log(firstName);
+// let firstName ="Jonas";
+// console.log(firstName);
 
-let age = 30;
-console.log(age);
-age = 50;
-console.log(age);
+// let age = 30;
+// console.log(age);
+// age = 50;
+// console.log(age);
 
-const birthYear = 1991;
-console.log(birthYear);
+// const birthYear = 1991;
+// console.log(birthYear);
 
-const PI = 3.1415;
-console.log(PI);
+// const PI = 3.1415;
+// console.log(PI);
+
+// var job ="programmer";
+// job = "teacher";
+// console.log (job); 'teacher'
+
+// age = 51;
+
+// let lastName = "Doe";
+
+// console.log("=== DATA TYPES ===");
+
+// age = 24;
+// console.log(age);
+// console.log(typeof age);
+
+// let username = "Marc";
+// console.log(username);
+// console.log(typeof username);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// let dynamicVariable = 23;
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+// dynamicVariable = "Now I'm a string!";
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+// dynamicVariable = true;
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+
+////////
+//Basic Operators
+console.log("=== MATH OPERATORS ===");
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageSarah / 2, 2 ** 3); // 2 ** 3 means 2 to the power of 3
